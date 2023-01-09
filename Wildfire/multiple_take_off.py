d38fe92949ecfec0b1679fad29df47f03ba60dd8
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2023-01-09 16:56:56 +0100

    Refactor

--- Wildfire/multiple_take_off.py
+++ Wildfire/multiple_take_off.py
@@ -21,7 +21,6 @@ async def run():
 
         print("Waiting for drone to have a global position estimate...")
         async for health in drone.telemetry.health():
-            print(health)
             if health.is_global_position_ok and health.is_home_position_ok:
                 print("-- Global position estimate OK")
                 break

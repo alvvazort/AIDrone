0e3015c77a5dbc9be57d65a6b589b799f266aeef
Gonzalo Martínez <80246529+gonmarmar5@users.noreply.github.com>
2023-01-24 17:28:34 +0100

    start_server with attributes

--- Wildfire/start_servers.sh
+++ Wildfire/start_servers.sh
@@ -1,3 +1,14 @@
-gnome-terminal -- sh -c './mavsdk_server -p 50051 udp://:14541' 
-gnome-terminal -- sh -c './mavsdk_server -p 50052 udp://:14542' 
-./mavsdk_server -p 50050 udp://:14540
+if [ -z  "$1" ]; then
+    numDrones=2
+else
+    numDrones=$(($1-1))
+fi
+
+for i in `seq 1 $numDrones`
+do
+    server_port=$((50050+$i))
+    udp_port=$((14540+$i))
+    gnome-terminal -- sh -c "./mavsdk_server -p $server_port udp://:$udp_port" 
+done
+
+./mavsdk_server -p 50050 udp://:14540 

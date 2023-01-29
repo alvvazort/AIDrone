b09db6ac04f6783def286f2770e73e2c137d1171
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2023-01-29 12:25:55 +0100

    Q-learning with MP working

--- Wildfire/wildfire_q-learning_MP.py
+++ Wildfire/wildfire_q-learning_MP.py
@@ -18,7 +18,7 @@ EPSILON = 0.9
 DISCOUNT_FACTOR = 0.9
 LEARNING_RATE = 0.9
 
-async def run():
+class Wildfire:
     
     latitude_origin = 0
     longitude_origin = 0
@@ -29,7 +29,7 @@ async def run():
     PC = Point(latitude_origin, longitude_origin)
     POINTS = {}
     q_values = {}
-    
+    actions_functions = []
     rewards = {}
 
     async def land_all(status_text_task):
@@ -73,59 +73,56 @@ async def run():
 
     def update_constants():
         def update_points():
-            PC = Point(latitude_origin, longitude_origin)
-            POINTS["PC"] = PC
+            PC = Point(Wildfire.latitude_origin, Wildfire.longitude_origin)
+            Wildfire.POINTS["PC"] = PC
 
             points = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
             for num_point in range(NUMPOINTS):
-                latitude_point = num_point * random.random() * 0.001 + latitude_origin
-                longitude_point = num_point * random.random() * 0.001  + longitude_origin
-                POINTS[points[num_point]] = Point(latitude_point, longitude_point)
+                latitude_point = num_point * random.random() * 0.001 + Wildfire.latitude_origin
+                longitude_point = num_point * random.random() * 0.001  + Wildfire.longitude_origin
+                Wildfire.POINTS[points[num_point]] = Point(latitude_point, longitude_point)
 
         def update_status():
-            for point in list(POINTS.keys()):
-                #TODO CAMBIAR A MAS ESTADOS
-                for battery_level in range(2,6):
+            for point in list(Wildfire.POINTS.keys()):
+                for battery_level in range(2,11):
                     status = point + str(battery_level)
                     STATUS.append(status)
         
         def update_q_values():
-            # Si existe el json con el qvalue lo carga
+            
+            # Si existe el json con los qvalues lo carga
             if os.path.isfile("q_values.json"):
                 with open("q_values.json") as json_file:
-                    q_values = json.load(json_file)
+                    Wildfire.q_values = json.load(json_file)
             else:
                 for status in STATUS:
-                    q_values[status]=np.zeros(len(POINTS)) # Un cero por cada go_to a cada punto -1 por go_to a si mismo + 1 por act
+                    Wildfire.q_values[status]=list(np.zeros(NUMPOINTS+2)) # Un cero por cada go_to a cada punto + 1 por act + 1 por PC
 
         def update_rewards():
+            
             for status in STATUS:
                 if status == "M":
-                    rewards[status]= -5000
+                    Wildfire.rewards[status]= -5000
                 elif "PC" in status or status == "F":
-                    rewards[status]= 0
+                    Wildfire.rewards[status]= 0
                 else:
-                    rewards[status]= 20
+                    Wildfire.rewards[status]= 20
                     
-        def update_actions():
-            #TODO
-            pass
-
+        def update_actions(): 
+            Wildfire.actions_functions.append("act")
+            for point in list(Wildfire.POINTS.keys()):
+                Wildfire.actions_functions.append("go_to_" + point)
+        
         update_points()
         update_status()
-        update_q_values()   
+        update_q_values()
         update_rewards()
         update_actions()
         
-    async def AIDrone(idDrone, episode):
-        #TODO CAMBIAR POR COMPLETO
-        async def go_to(idDrone):
+    async def AIDrone(idDrone,drone, episode):
 
-            last_point = record[idDrone][-1]
-            if(last_point=="PC"): #Si está en un punto, va hacia el otro punto 
-                point= POINTS["A"]
-            else:
-                point= POINTS["PC"]
+        async def go_to(idDrone, point):
+
             global is_flying
             if not is_flying:
                 print("-- Arming")
@@ -133,22 +130,22 @@ async def run():
                 print("-- Taking off")
                 await drone.action.takeoff()
                 
-            await drone.action.goto_location(point.latitude_deg, point.longitude_deg, flying_alt, 0)
+            await drone.action.goto_location(point.latitude_deg, point.longitude_deg, Wildfire.flying_alt, 0)
             is_flying=True
 
-            battery = round(await get_battery(drone)*100,2)    
-            name_point = [k for k, v in POINTS.items() if v == point][0]
+            battery = round(await Wildfire.get_battery(drone)*100,2)    
+            name_point = [k for k, v in Wildfire.POINTS.items() if v == point][0]
             print("Going to " + name_point + " with " + str(battery) + " percentage at " + str(datetime.datetime.now().strftime('%H:%M:%S')) + " (" + await get_status() + ")")
             
             async for position in drone.telemetry.position():
                 #Comprueba que llega al punto    
                 if abs(position.latitude_deg-point.latitude_deg)<0.00001 and abs(position.longitude_deg-point.longitude_deg)<0.00001: 
-                    record[idDrone].append(name_point) # Guarda en el historial en que punto está
+                    Wildfire.record[idDrone].append(name_point) # Guarda en el historial en que punto está
                     break
         
         async def act(idDrone):
-            actual_point = record[idDrone][-1]
-            battery = round(await get_battery(drone)*100,2)
+            actual_point = Wildfire.record[idDrone][-1]
+            battery = round(await Wildfire.get_battery(drone)*100,2)
             actual_status = await get_status()    
             global is_flying
 
@@ -160,38 +157,37 @@ async def run():
                     i=0
                     async for in_air_local in drone.telemetry.in_air():
                         if(i%20==0):
-                            print("Trying to land, still in air. Still " + str(round(await get_altitude(drone),2)) + " from ground.")
+                            print("Trying to land, still in air. Still " + str(round(await Wildfire.get_altitude(drone),2)) + " from ground.")
                         i = i+1
                         if not in_air_local:
                             is_flying=False
                             break
-                print("Charging battery at " + actual_point + " with " + str(round(await get_battery(drone)*100,2)) + " percentage at " + str(datetime.datetime.now().strftime('%H:%M:%S')) + " (" + await get_status() + ")")
+                print("Charging battery at " + actual_point + " with " + str(round(await Wildfire.get_battery(drone)*100,2)) + " percentage at " + str(datetime.datetime.now().strftime('%H:%M:%S')) + " (" + await get_status() + ")")
 
             else:
                 print("Monitoring point " + actual_point + " with " + str(battery) + " percentage at " + str(datetime.datetime.now().strftime('%H:%M:%S')) + " (" + actual_status + ")")
-                await drone.action.do_orbit(radius_m=2.0, velocity_ms=10.0, yaw_behavior = OrbitYawBehavior.HOLD_FRONT_TO_CIRCLE_CENTER, latitude_deg = POINTS[actual_point].latitude_deg, longitude_deg = POINTS[actual_point].longitude_deg, absolute_altitude_m = absolute_altitude_origin + 20)
+                await drone.action.do_orbit(radius_m=2.0, velocity_ms=10.0, yaw_behavior = OrbitYawBehavior.HOLD_FRONT_TO_CIRCLE_CENTER, latitude_deg = Wildfire.POINTS[actual_point].latitude_deg, longitude_deg = Wildfire.POINTS[actual_point].longitude_deg, absolute_altitude_m = Wildfire.absolute_altitude_origin + 20)
                 await asyncio.sleep(10)
 
-            record[idDrone].append(actual_point)
+            Wildfire.record[idDrone].append(actual_point)
             
         async def get_battery_status(drone):
-            battery= await get_battery(drone)
-            # TODO CAMBIAR A MAS ESTADOS = {1 : 0.16, 2: 0.25, 3: 0.35, 4: 0.45, 5:0.55 , 6: 0.65, 7: 0.75, 8: 0.85, 9: 0.95 , 10: 1.0}
-            battery_levels = {1 : 0.16, 2: 0.45, 3: 0.60, 4: 0.8, 5: 1.0}
-            
+            battery= await Wildfire.get_battery(drone)
+            #battery_levels = {1 : 0.16, 2: 0.45, 3: 0.60, 4: 0.8, 5: 1.0}
+            battery_levels = {1 : 0.16, 2: 0.25, 3: 0.35, 4: 0.45, 5:0.55 , 6: 0.65, 7: 0.75, 8: 0.85, 9: 0.95 , 10: 1.0}
             battery_status = [k for k, v in battery_levels.items() if v >= battery][0]
             return battery_status
 
         async def get_status():
             battery_status = await get_battery_status(drone)
-            point = record[idDrone][-1]
+            point = Wildfire.record[idDrone][-1]
             global is_flying
 
             if(battery_status==1):
                 if(point=="M"):
                     return "F"
                 if is_flying:
-                    record[idDrone].append("M")
+                    Wildfire.record[idDrone].append("M")
                     print("Mayday! Mayday! Drone without battery " + "(M)")
 
                     return "M"
@@ -202,22 +198,26 @@ async def run():
         def get_next_action(state, epsilon):
             #if a randomly chosen value between 0 and 1 is less than epsilon, 
             #then choose the most promising value from the Q-table for this state.
-            
             if np.random.random() < epsilon:
-                return np.argmax(q_values[state])
+                return np.argmax(Wildfire.q_values[state])
             else: #choose a random action
-                return np.random.randint(2)
+                return np.random.randint(NUMPOINTS+2) # Un posibilidad por cada go_to a cada punto -1 por go_to a si mismo + 1 por act + 1 por PC
         
-        async def get_next_status(action_index):
-            action=actions_functions[action_index]
-            await action(idDrone)
+        async def get_next_status(action_index, idDrone):
+            action=Wildfire.actions_functions[action_index]
+            if(action=="act"):
+                await act(idDrone)
+            else: 
+                point_string= action.split("_")[-1]
+                point = Wildfire.POINTS[point_string]
+                await go_to(idDrone, point)
             return await get_status()
 
         async def reset_episode(drone, episode):
-            await drone.action.goto_location(POINTS["PC"].latitude_deg, POINTS["PC"].longitude_deg, flying_alt, 0)
+            await drone.action.goto_location(Wildfire.POINTS["PC"].latitude_deg, Wildfire.POINTS["PC"].longitude_deg, Wildfire.flying_alt, 0)
             async for position in drone.telemetry.position():
                 #Comprueba que llega al punto    
-                if abs(position.latitude_deg-POINTS["PC"].latitude_deg)<0.00001 and abs(position.longitude_deg-POINTS["PC"].longitude_deg)<0.00001: 
+                if abs(position.latitude_deg-Wildfire.POINTS["PC"].latitude_deg)<0.00001 and abs(position.longitude_deg-Wildfire.POINTS["PC"].longitude_deg)<0.00001: 
                     break
             
             # Aterriza y carga la batería
@@ -232,14 +232,13 @@ async def run():
             await asyncio.sleep(4)
             print("Starting new episode - Episode " + str(episode+1))
 
-        #TODO CAMBIAR A MAS FUNCIONES
-        actions_functions = [act,go_to]
         global is_flying
         is_flying = True
 
         status = await get_status()
 
         while(status != "M"):
+            
             status = await get_status()
 
             action_index=get_next_action(status, EPSILON)
@@ -247,56 +246,56 @@ async def run():
             #perform the chosen action, and transition to the next state (i.e., move to the next location)
             old_status = status #store the old row and column indexes
 
-            status = await get_next_status(action_index)
+            status = await get_next_status(action_index, idDrone)
 
             #receive the reward for moving to the new state, and calculate the temporal difference
-            reward = rewards[status]
-            old_q_value = q_values[old_status][action_index]
-            temporal_difference = reward + (DISCOUNT_FACTOR * np.max(q_values[status])) - old_q_value
+            reward = Wildfire.rewards[status]
+
+            old_q_value = Wildfire.q_values[old_status][action_index]
+            temporal_difference = reward + (DISCOUNT_FACTOR * np.max(Wildfire.q_values[status])) - old_q_value
             
             new_q_value = old_q_value + (LEARNING_RATE * temporal_difference)
-            q_values[old_status][action_index] = new_q_value #actualización
+            Wildfire.q_values[old_status][action_index] = new_q_value #actualización
             with open("q_values.json", 'w') as outfile:
-                json.dump(q_values, outfile) #actualizacion en json
+                json.dump(Wildfire.q_values, outfile) #actualización en json
 
         await reset_episode(drone, episode)
 
-        
-    ### Código a ejecutar en run() ###
+    
+    async def run():
+        print("Drone 0")
+        drone = System()
+        await drone.connect(system_address="udp://:"+str(PORT))
 
-    print("Drone 0")
-    drone = System()
-    await drone.connect(system_address="udp://:"+str(PORT))
+        #status_text_task = asyncio.ensure_future(print_status_text(drone))
 
-    status_text_task = asyncio.ensure_future(print_status_text(drone))
+        print("Waiting for drone to have a global position estimate...")
+        async for health in drone.telemetry.health():
+            if health.is_global_position_ok and health.is_home_position_ok:
+                print("-- Global position estimate OK")
+                break
 
-    print("Waiting for drone to have a global position estimate...")
-    async for health in drone.telemetry.health():
-        if health.is_global_position_ok and health.is_home_position_ok:
-            print("-- Global position estimate OK")
+        print("Fetching home location coordinates and altitude...")
+        async for terrain_info in drone.telemetry.home():
+            Wildfire.latitude_origin = terrain_info.latitude_deg
+            Wildfire.longitude_origin = terrain_info.longitude_deg
+            Wildfire.absolute_altitude_origin = terrain_info.absolute_altitude_m
+            Wildfire.flying_alt = Wildfire.absolute_altitude_origin + 40
             break
 
-    print("Fetching home location coordinates and altitude...")
-    async for terrain_info in drone.telemetry.home():
-        latitude_origin = terrain_info.latitude_deg
-        longitude_origin = terrain_info.longitude_deg
-        absolute_altitude_origin = terrain_info.absolute_altitude_m
-        flying_alt = absolute_altitude_origin + 40
-        break
+        Wildfire.update_constants()
 
-    update_constants()
+        Wildfire.record.append([])
+        for episode in range(20):
+            Wildfire.record[0].append("PC")
+            print("-- Arming")
+            await drone.action.arm()
+            
+            print("-- Taking off")
+            await drone.action.takeoff()
+            await Wildfire.AIDrone(0,drone,episode)    # 0 es idDrone
 
-    record.append([])
-    for episode in range(20):
-        record[0].append("PC")
-        print("-- Arming")
-        await drone.action.arm()
-        
-        print("-- Taking off")
-        await drone.action.takeoff()
-        await AIDrone(0,episode)
-
-    print("Training completed")
+        print("Training completed")
 
 
 async def print_status_text(drone):
@@ -317,4 +316,5 @@ async def get_drones():
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    #loop.run_until_complete(Wildfire.calculate_coordinates())
+    loop.run_until_complete(Wildfire.run())
